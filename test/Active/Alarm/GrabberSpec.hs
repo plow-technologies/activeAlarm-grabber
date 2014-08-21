1bc15bc6688e3a73b719a103467b57b4a23ca694
Lingpo Huang <lingpohuang7@gmail.com>
2014-08-21 10:53:09 -0500

    Add test functions.

--- test/Active/Alarm/GrabberSpec.hs
+++ test/Active/Alarm/GrabberSpec.hs
@@ -1,3 +1,15 @@
+{- |
+Module      :  <Active.Alarm.GrabberSpec>
+Description :  <Spec for Grabber>
+Copyright   :  (c) <Plow Technology 2014>
+License     :  <MIT>
+
+Maintainer  :  <lingpo.huang@plowtech.net>
+Stability   :  unstable
+Portability :  portable 
+
+<Spec Function to test the des and host pair matching function is working correctly>
+-}
 module Active.Alarm.GrabberSpec (main, spec) where
 
 import Test.Hspec
@@ -8,6 +20,10 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "someFunction" $ do
-    it "should work fine" $ do
-      True `shouldBe` False
+  describe "MatchKeys" $ do
+    it "Create a list of des and host pair that shouldbe exact." $ do
+      matchKeys ([1..13] :: [Int]) [1] `shouldBe` (\xs ys -> [(x, y) | x <- xs, y <-ys]) ([1..13]::[Int]) [1]
+    it "Create a list of des and host pair that should be exactly these pairs." $ do
+      matchKeys [1..5] [1..2] `shouldBe` [(1,1),(1,2),(2,1),(2,2),(3,1),(3,2),(4,1),(4,2),(5,1),(5,2)]
+    it "Create a list of des and host pair that shouldbe the same size of the product of list 1 size and list 2 size." $ do
+      length (matchKeys ([1..10] :: [Int]) [1..4]) `shouldBe` ((length [1..10]) * (length [1..4]))
